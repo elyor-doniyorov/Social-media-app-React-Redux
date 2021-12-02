3ab3c40b59d06ddd7078b2f65e975c5f4bcda3e2
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-02 21:20:43 +0500

    Structure folders and style components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => (
   <div className="app-wrapper">
-    <header className="header">
-      <img src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-transparent-png-19.png" alt="logo" />
-    </header>
-    <nav className="nav">
-      <div>
-        <a href="#s">Profile</a>
-      </div>
-      <div>
-        <a href="#s">Messages</a>
-      </div>
-      <div>
-        <a href="#s">News</a>
-      </div>
-      <div>
-        <a href="#s">Music</a>
-      </div>
-      <div>
-        <a href="#s">Settings</a>
-      </div>
-    </nav>
-    <div className="content">
-      <div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyVQ31aLZ4MtwTqvDChFTgI1mp8Duma8StXA&usqp=CAU" alt="profile" />
-      </div>
-      <div>
-        ava + description
-      </div>
-      <div>
-        My posts
-        <div>
-          New post
-        </div>
-      </div>
-      <div>
-        <div>
-          Post 1
-        </div>
-        <div>
-          Post 2
-        </div>
-      </div>
-    </div>
+    <Header />
+    <Navbar />
+    <Profile />
   </div>
 );
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    {/* <img src="https://www.freepnglogos.com/uploads/logo-tokopedia-png/tokopedia-apa-itu-startup-pengertian-cara-memulai-dan-1.png" alt="logo" /> */}
+    <h3 className="main-title">Gashtak Vibes</h3>
+  </header>
+);
+
+export default Header;

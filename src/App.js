c6344cd158b4de877c60e5329d5516030e0a5f5f
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-02 16:47:28 +0500

    Delete Header, Footer, Technologies components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => (
   <div className="App">
-    <Header />
-    <Technologies />
-    <Footer />
+    hello
   </div>
 );
 

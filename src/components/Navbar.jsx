74184577e9de844d690d35ef90395e6b462ba6c9
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-03 22:22:02 +0500

    Style components using css modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable */
 import React from 'react';
+import s from './Navbar.module.css';
 
 const Navbar = () => (
-  <nav className="nav">
-    <div>
+  <nav className={s.nav}>
+    <div className={`${s.item} ${s.active}`}>
       <a href="#s">Profile</a>
     </div>
-    <div>
+    <div className={s.item}>
       <a href="#s">Messages</a>
     </div>
-    <div>
+    <div className={s.item}>
       <a href="#s">News</a>
     </div>
-    <div>
+    <div className={s.item}>
       <a href="#s">Music</a>
     </div>
-    <div>
+    <div className={s.item}>
       <a href="#s">Settings</a>
     </div>
   </nav>

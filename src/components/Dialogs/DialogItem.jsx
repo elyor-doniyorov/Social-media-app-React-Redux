3a17ac0e1568c6e7d7c77968bbb23578ca05b9f5
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-04 20:24:42 +0500

    Refactor dialogs code

---
+++ src/components/Dialogs/DialogItem.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import s from './Dialogs.module.css';
+
+const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+  return (
+    <div className={s.dialog}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+export default DialogItem;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
+import DialogItem from './DialogItem';
+import Message from './Message';
 
 const Dialogs = () => (
   <div className={s.dialogs}>
     <div className={s.dialogs_items}>
-      <div className={s.dialog}>
-        <NavLink to="/dialogs/1">Elyor</NavLink>
-      </div>
-      <div className={s.dialog}>
-        <NavLink to="/dialogs/2">Avazbek</NavLink>
-      </div>
-      <div className={s.dialog}>
-        <NavLink to="/dialogs/3">Dildora</NavLink>
-      </div>
-      <div className={s.dialog}>
-        <NavLink to="/dialogs/4">Bahrom</NavLink>
-      </div>
-      <div className={s.dialog}>
-        <NavLink to="/dialogs/5">Abbos</NavLink>
-      </div>
-      <div className={s.dialog}>
-        <NavLink to="/dialogs/6">Farhod</NavLink>
-      </div>
+      <DialogItem name="Elyor" id="1" />
+      <DialogItem name="Avazbek" id="2" />
+      <DialogItem name="Dildora" id="3" />
+      <DialogItem name="Bahrom" id="4" />
+      <DialogItem name="Abbos" id="5" />
+      <DialogItem name="Farhod" id="6" />
     </div>
     <div className={s.messages}>
-      <div className={s.message}>Hi!</div>
-      <div className={s.message}>How are you?</div>
-      <div className={s.message}>What about you?</div>
+      <Message message="HI" />
+      <Message message="How are you?" />
+      <Message message="What about you?" />
+      <Message message="Yeah?" />
+      <Message message="Yo" />
     </div>
   </div>
 );

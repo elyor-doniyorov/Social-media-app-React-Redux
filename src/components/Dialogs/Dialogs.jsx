99971fa647ca9e370b0a2e01c97369750ed71054
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-04 14:40:14 +0500

    Add Dialogs page

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+const Dialogs = () => (
+  <div className={s.content}>
+    Dialogs
+  </div>
+);
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
 const Profile = () => (
-  <div className={s.content}>
+  <div className={s.profile}>
     <div>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyVQ31aLZ4MtwTqvDChFTgI1mp8Duma8StXA&usqp=CAU" alt="profile" />
     </div>

35ddf33e045de444206fd164b56ecf2d5089994e
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-04 18:12:55 +0500

    Style dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import s from './Dialogs.module.css';
 
 const Dialogs = () => (
-  <div className={s.content}>
-    Dialogs
+  <div className={s.dialogs}>
+    <div className={s.dialogs_items}>
+      <div className={s.dialog}>
+        Elyor
+      </div>
+      <div className={s.dialog}>
+        Avazbek
+      </div>
+      <div className={s.dialog}>
+        Dildora
+      </div>
+      <div className={s.dialog}>
+        NoName
+      </div>
+      <div className={s.dialog}>
+        NoName
+      </div>
+      <div className={s.dialog}>
+        NoName
+      </div>
+    </div>
+    <div className={s.messages}>
+      <div className={s.message}>Hi!</div>
+      <div className={s.message}>How are you?</div>
+      <div className={s.message}>What about you?</div>
+    </div>
   </div>
 );
 

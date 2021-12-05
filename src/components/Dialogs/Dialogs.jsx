345c8162fdd4b15290e77ffc76529211bc75d764
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-05 18:26:53 +0500

    Understatnd UI-BLL concepts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,37 @@
+/* eslint-disable */
 import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem';
 import Message from './Message';
 
+let dialogsData = [
+  { id: 1, name: "Elyor" },
+  { id: 2, name: "Avazbek" },
+  { id: 3, name: "Dildora" },
+  { id: 4, name: "Bahrom" },
+  { id: 5, name: "Abbos" },
+  { id: 6, name: "Farhod" }
+];
+
+let messagesData = [
+  { id: 1, message: "HI" },
+  { id: 2, message: "How are you?" },
+  { id: 3, message: "What about you?" },
+  { id: 4, message: "Yeah?" },
+  { id: 5, message: "Yo" }
+];
+
 const Dialogs = () => (
   <div className={s.dialogs}>
     <div className={s.dialogs_items}>
-      <DialogItem name="Elyor" id="1" />
-      <DialogItem name="Avazbek" id="2" />
-      <DialogItem name="Dildora" id="3" />
-      <DialogItem name="Bahrom" id="4" />
-      <DialogItem name="Abbos" id="5" />
-      <DialogItem name="Farhod" id="6" />
+      <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+      <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+      <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
     </div>
     <div className={s.messages}>
-      <Message message="HI" />
-      <Message message="How are you?" />
-      <Message message="What about you?" />
-      <Message message="Yeah?" />
-      <Message message="Yo" />
+      <Message message={messagesData[0].message} />
+      <Message message={messagesData[1].message} />
+      <Message message={messagesData[2].message} />
     </div>
   </div>
 );

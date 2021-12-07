708515a1962de7b20bec522c28637914f5525f34
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-07 18:25:44 +0500

    packing data into state

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ const App = (props) => {
         <Navbar />
         <div className="app-wrapper-content">
           <Routes>
-            <Route path="/profile" element={<Profile posts={props.posts} />} />
-            <Route exact path="/dialogs" element={<Dialogs dialogs={props.dialogs} messages={props.messages} />} />
+            <Route path="/profile" element={<Profile posts={props.state.posts} />} />
+            <Route exact path="/dialogs" element={<Dialogs dialogs={props.state.dialogs} messages={props.state.messages} />} />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} />
--- src/index.js
+++ src/index.js
@@ -2,35 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-const posts = [
-  { id: 1, message: 'Why nobody loves me?', likesCount: 0 },
-  { id: 2, message: 'Hi everyone!', likesCount: 10 },
-  { id: 3, message: 'I am Elyor', likesCount: 12 },
-  { id: 4, message: 'I want to earn $1000 per month', likesCount: 20 },
-  { id: 5, message: 'How to improve logical thinking?', likesCount: 9 },
-];
-
-const dialogs = [
-  { id: 1, name: 'Elyor' },
-  { id: 2, name: 'Avazbek' },
-  { id: 3, name: 'Dildora' },
-  { id: 4, name: 'Bahrom' },
-  { id: 5, name: 'Abbos' },
-  { id: 6, name: 'Farhod' },
-];
-
-const messages = [
-  { id: 1, message: 'HI' },
-  { id: 2, message: 'How are you?' },
-  { id: 3, message: 'What about you?' },
-  { id: 4, message: 'Yeah?' },
-  { id: 5, message: 'Yo' },
-];
+import state from './redux/State';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages} />
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root'),
 );
